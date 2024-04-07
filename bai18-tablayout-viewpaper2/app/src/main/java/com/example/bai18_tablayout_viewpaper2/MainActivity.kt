bf63c6e7063be3c8436302607fe83f592d6581df
VietHoang0805 <hoangviet852004@gmail.com>
2024-04-07 16:05:25 +0700

    Push bai18 full

--- bai18-tablayout-viewpaper2/app/src/main/java/com/example/bai18_tablayout_viewpaper2/MainActivity.kt
+++ bai18-tablayout-viewpaper2/app/src/main/java/com/example/bai18_tablayout_viewpaper2/MainActivity.kt
@@ -11,17 +11,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentActivity
 import com.example.bai18_tablayout_viewpaper2.databinding.ActitvityMainBinding
 import com.example.bai18_tablayout_viewpaper2.ui.theme.Bai18tablayoutviewpaper2Theme
+import com.google.android.material.tabs.TabLayoutMediator
 
 @SuppressLint("StaticFieldLeak")
 private lateinit var binding: ActitvityMainBinding
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Khởi tạo viewbinding
         binding = ActitvityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val adaptervv = ViewPagerAdapter(supportFragmentManager,lifecycle)
+        binding.pagerTest.adapter = adaptervv
+        TabLayoutMediator(binding.tabDemo, binding.pagerTest) {tab,pos->
+            when(pos) {
+                0->{tab.text="Tab 1"}
+                1->{tab.text="Tab 2"}
+                2->{tab.text="Tab 3"}
+                3->{tab.text="Tab 4"}
+                4->{tab.text="Tab 5"}
+            }
+        }.attach()
     }
 }
 
--- bai18-tablayout-viewpaper2/app/src/main/java/com/example/bai18_tablayout_viewpaper2/ViewPagerAdapter.kt
+++ bai18-tablayout-viewpaper2/app/src/main/java/com/example/bai18_tablayout_viewpaper2/ViewPagerAdapter.kt
@@ -1,4 +1,25 @@
 package com.example.bai18_tablayout_viewpaper2
 
-class ViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStateManagerControl
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle):
+    FragmentStateAdapter (fragmentManager,lifecycle) {
+    // Ctrl i
+    override fun getItemCount(): Int {
+        return 5
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0->{fragment1()}
+            1->{fragment2()}
+            2->{fragment3()}
+            3->{fragment4()}
+            else->{fragment5()}
+        }
+    }
 }
