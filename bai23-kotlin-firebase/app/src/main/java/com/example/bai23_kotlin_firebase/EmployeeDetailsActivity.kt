43a2e41d6da4872016cae10b80c56a88638c0a4f
VietHoang0805 <hoangviet852004@gmail.com>
2024-04-16 22:25:36 +0700

    Push full bai 23

--- bai23-kotlin-firebase/app/src/main/java/com/example/bai23_kotlin_firebase/EmployeeDetailsActivity.kt
+++ bai23-kotlin-firebase/app/src/main/java/com/example/bai23_kotlin_firebase/EmployeeDetailsActivity.kt
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.bai23_kotlin_firebase.databinding.ActivityEmployeeDetailsBinding
 import com.google.firebase.database.FirebaseDatabase
 
@@ -24,7 +27,61 @@ class EmployeeDetailsActivity : AppCompatActivity() {
                 intent.getStringExtra("empId").toString()
             )
         }
+
+        // Code cho button update
+        binding.btnUpdate.setOnClickListener {
+            openUpdateDialog(
+                intent.getStringExtra("empId").toString(),
+                intent.getStringExtra("empName").toString()
+            )
+        }
     }
+
+    private fun openUpdateDialog(empId: String, empName: String) {
+        val mDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val mDialogView = inflater.inflate(R.layout.update_dialog,null)
+        mDialog.setView(mDialogView)
+        // update thông tin vào dialog
+        val etEmpName = mDialogView.findViewById<EditText>(R.id.etEmpName)
+        val etEmpAge = mDialogView.findViewById<EditText>(R.id.etEmpAge)
+        val etEmpSalary = mDialogView.findViewById<EditText>(R.id.etEmpSalary)
+        val btnUpdateData = mDialogView.findViewById<Button>(R.id.btnUpdateData)
+
+        etEmpName.setText(intent.getStringExtra("empName").toString())
+        etEmpAge.setText(intent.getStringExtra("empAge").toString())
+        etEmpSalary.setText(intent.getStringExtra("empSalary").toString())
+
+        mDialog.setTitle("Updating $empName Record")
+        val alertDialog = mDialog.create()
+        alertDialog.show()
+        // Set sự kiện cho btnUpdate Data
+        btnUpdateData.setOnClickListener {
+            updateEmpData(
+                empId,
+                etEmpName.text.toString(),
+                etEmpAge.text.toString(),
+                etEmpSalary.text.toString()
+            )
+            Toast.makeText(applicationContext,"Employee Data updated",Toast.LENGTH_SHORT).show()
+            // update lại data lên hộp thoại dialog
+            binding.tvEmpName.setText(etEmpName.text.toString())
+            binding.tvEmpAge.setText(etEmpAge.text.toString())
+            binding.tvEmpSalary.setText(etEmpSalary.text.toString())
+            alertDialog.dismiss()
+        }
+    }
+
+    private fun updateEmpData(
+        id: String,
+        name: String,
+        age: String,
+        salary: String) {
+        val dbRef = FirebaseDatabase.getInstance().getReference("Employees").child(id)
+        val empInfo = EmployeeModel(id,name,age,salary)
+        dbRef.setValue(empInfo)
+    }
+
 
     private fun deleteRecord(id: String) {
         val dbRef = FirebaseDatabase.getInstance().getReference("Employees").child(id)
