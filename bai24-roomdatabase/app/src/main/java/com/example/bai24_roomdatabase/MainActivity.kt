4a3e6ee4d61ddb8dbb14ba144125386b6b0bcf52
VietHoang0805 <hoangviet852004@gmail.com>
2024-04-24 17:09:44 +0700

    Push set up bai24-roomdatabase

---
+++ bai24-roomdatabase/app/src/main/java/com/example/bai24_roomdatabase/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.bai24_roomdatabase
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bai24_roomdatabase.databinding.ActivityMainBinding
+import com.example.bai24_roomdatabase.ui.theme.Bai24roomdatabaseTheme
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding: ActivityMainBinding
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Khởi tạo viewbinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
